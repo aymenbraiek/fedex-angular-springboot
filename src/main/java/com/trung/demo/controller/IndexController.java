a077fc7eeeb9b302d6d479557b43b94e721415fc
trungvo <vtt311096@gmail.com>
2019-11-24 12:49:43 -0500

    Validate login backend API with LoginService

--- src/main/java/com/trung/demo/controller/IndexController.java
+++ src/main/java/com/trung/demo/controller/IndexController.java
@@ -1,13 +1,30 @@
 package com.trung.demo.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.trung.demo.services.LoginService;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class IndexController {
+	
+	@Autowired
+	public LoginService loginService;
 	
 	@RequestMapping("/")
 	public String index() {
 		return "Hello world!!!";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json")
+	public Map<String, Object> validateLogin(@RequestBody Map<String, String> credentials) {
+		return loginService.validateLogin(credentials);
 	}
 }
---
+++ src/main/java/com/trung/demo/services/LoginService.java
@@ -0,0 +1,39 @@
+package com.trung.demo.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.trung.demo.model.User;
+import com.trung.demo.repository.UserRepository;
+
+@Service
+public class LoginService {
+	@Autowired
+	private UserRepository userRepo;
+	
+	public Map<String, Object> validateLogin(Map<String, String> credentials) {
+		Map<String, Object> res = new HashMap<>();
+		
+		if (!userRepo.existsByUserName(credentials.get("userName"))) {
+			res.put("valid", false);
+			res.put("userNameErrMsg", "Non-exist username");
+			res.put("passwordErrMsg", null);
+		} else {
+			User foundUser = userRepo.findByUserName(credentials.get("userName"));
+			if (foundUser.getPassword().equals(credentials.get("password"))) {
+				res.put("valid", true);
+				res.put("userNameErrMsg", null);
+				res.put("passwordErrMsg", null);
+			} else {
+				res.put("valid", false);
+				res.put("userNameErrMsg", null);
+				res.put("passwordErrMsg", "Incorrect password");
+			}
+		}
+		
+		return res;
+	}
+}
