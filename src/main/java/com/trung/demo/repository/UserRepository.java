67cbae8f728b38974f63a4f9017260fb766ceed8
trungvo <vtt311096@gmail.com>
2019-12-02 16:55:07 -0500

    Added @Repository in UserRepository

--- src/main/java/com/trung/demo/repository/UserRepository.java
+++ src/main/java/com/trung/demo/repository/UserRepository.java
@@ -1,8 +1,12 @@
+
+
 package com.trung.demo.repository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.trung.demo.model.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, String> {
 	User findByEmail(String email);
 	boolean existsByEmail(String email);
