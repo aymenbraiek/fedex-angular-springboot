b7d0c85920bc44b7f3efa398e1dcb70357811cbd
trungvo <vtt311096@gmail.com>
2019-12-05 21:16:42 -0500

    [IMPORTANT] Add http.cors() to exclude preflight request in Spring Security

--- src/main/java/com/trung/demo/security/SecurityConfig.java
+++ src/main/java/com/trung/demo/security/SecurityConfig.java
@@ -35,23 +35,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(myUserDetailsService);
 		
 		// setup default admin & user for the app
-		String encodedPass = new BCryptPasswordEncoder().encode("pass");
-		User admin = new User("Phuong", "Chu", "aiko@gmail.com", encodedPass, encodedPass);
-		admin.addRole(new Role("ADMIN", admin));
-		userRepository.save(admin);
-		
-		User admin2 = new User("Trung", "Vo", "vtt311096@gmail.com", encodedPass, encodedPass);
-		admin2.addRole(new Role("ADMIN", admin2));
-		userRepository.save(admin2);
-		
-		User emp1 = new User("Quang", "Vo", "vtq3008@gmail.com", encodedPass, encodedPass);
-		emp1.addRole(new Role("EMPLOYEE", emp1));
-		emp1.addRole(new Role("ADMIN", emp1));
-		userRepository.save(emp1);
-		
-		User user = new User("Andrew", "White", "andrew@gmail.com", encodedPass, encodedPass);
-		user.addRole(new Role("CUSTOMER", user));
-		userRepository.save(user);
+//		String encodedPass = new BCryptPasswordEncoder().encode("pass");
+//		User admin = new User("Phuong", "Chu", "aiko@gmail.com", encodedPass, encodedPass);
+//		admin.addRole(new Role("ADMIN", admin));
+//		userRepository.save(admin);
+//		
+//		User admin2 = new User("Trung", "Vo", "vtt311096@gmail.com", encodedPass, encodedPass);
+//		admin2.addRole(new Role("ADMIN", admin2));
+//		userRepository.save(admin2);
+//		
+//		User emp1 = new User("Quang", "Vo", "vtq3008@gmail.com", encodedPass, encodedPass);
+//		emp1.addRole(new Role("EMPLOYEE", emp1));
+//		emp1.addRole(new Role("ADMIN", emp1));
+//		userRepository.save(emp1);
+//		
+//		User user = new User("Andrew", "White", "andrew@gmail.com", encodedPass, encodedPass);
+//		user.addRole(new Role("CUSTOMER", user));
+//		userRepository.save(user);
 	}
 	
 	
@@ -66,6 +66,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated()
 			.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		// excluded the preflight requests from authorization 
+		// our API expects an authorization token in the OPTIONS request as well
+		// therefore, we need to exclude OPTIONS requests from authorization checks
+		http.cors();
 		
 		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}	
